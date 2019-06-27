545c7fc1f5540b310d1436e0d0abf6f3cd488ef8
Martin Torhage <martin@arweave.org>
2019-06-27 16:55:56 +0200

    Decrease dataset to 0.5 GiB for lower mem usage

--- src/configuration.h
+++ src/configuration.h
@@ -47,7 +47,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define RANDOMX_SUPERSCALAR_LATENCY   170
 
 //Dataset base size in bytes. Must be a power of 2.
-#define RANDOMX_DATASET_BASE_SIZE  1073741824 // 2^30, ~1GB, tweaked for Arweave
+#define RANDOMX_DATASET_BASE_SIZE  536870912 // 2^29 = 512 Mi (bytes), tweaked for Arweave
 
 //Dataset extra size. Must be divisible by 64.
 #define RANDOMX_DATASET_EXTRA_SIZE 31563008 // 493172 (just an arbitrary number) * 64, tweaked for Arweave
